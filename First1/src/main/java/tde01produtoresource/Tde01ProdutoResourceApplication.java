386e555e81e5ee0ba6c0788a3d97441acfd6aeab
MathzG <56983396+MathzG@users.noreply.github.com>
2021-08-23 23:28:50 -0300

    Spring-webseguro

---
+++ First1/src/main/java/tde01produtoresource/Tde01ProdutoResourceApplication.java
@@ -0,0 +1,12 @@
+package tde01produtoresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tde01ProdutoResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Tde01ProdutoResourceApplication.class, args);
+    }
+
+}
